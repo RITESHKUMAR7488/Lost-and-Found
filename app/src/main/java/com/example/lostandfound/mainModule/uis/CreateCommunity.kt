f03c45b91ca8c6851bfbac8e53fb5d7fa6c5284a
RITESHKUMAR7488 <riteshman30@gmail.com>
2025-06-29 19:03:20 +0530

    Main Module SetUp

--- app/src/main/java/com/example/lostandfound/mainModule/uis/CreateCommunity.kt
+++ app/src/main/java/com/example/lostandfound/mainModule/uis/CreateCommunity.kt
@@ -5,17 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import com.example.lostandfound.R
+import com.example.lostandfound.databinding.ActivityCreateCommunityBinding
 import com.example.lostandfound.utility.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CreateCommunity : BaseActivity() {
-    private lateinit var binding:A
+    private lateinit var binding:ActivityCreateCommunityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_create_community)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_create_community)
 
     }
 }
